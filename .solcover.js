666b81722fc1d95041b2892e4e117f23ef863cde
yorhodes <yorkerhodesiv@gmail.com>
2019-04-26 00:15:10 -0500

    delete dot files

--- .solcover.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  port: 8555,
-  // only use a few accounts
-  accounts: 10,
-  // use the local version of truffle
-  testCommand: '../node_modules/.bin/truffle test --network coverage -p 7545'
-};
